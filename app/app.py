f666f89a4767c5f6f96bded9393bfbcdbcd02e51
Edmond Legaspi <legaspi.edmond@gmail.com>
2022-08-26 09:45:13 +0800

    Added port 5000

--- app/app.py
+++ app/app.py
@@ -22,6 +22,7 @@ def search():
         return render_template('error.html')
 
 if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
     app.run(debug=True)
